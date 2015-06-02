44461c112db848372d7abe7747ad2b17a79365cf
mistr33 <mironovskiy.r@gmail.com>
2015-06-02 21:06:04 +0300

    alpha

--- js/my-app.js
+++ js/my-app.js
@@ -1,154 +1,48 @@
 // Initialize your app
-var myApp = new Framework7({
-    animateNavBackIcon: true,
-    // Enable templates auto precompilation
-    precompileTemplates: true,
-    // Enabled pages rendering using Template7
-	swipeBackPage: true,
-	pushState: true,
-    template7Pages: true
-});
+var myApp = new Framework7();
 
 // Export selectors engine
 var $$ = Dom7;
 
-// Add main View
+// Add view
 var mainView = myApp.addView('.view-main', {
-    // Enable dynamic Navbar
-    dynamicNavbar: false,
+    // Because we use fixed-through navbar we can enable dynamic navbar
+    dynamicNavbar: true
 });
-$$(document).on('pageInit', function (e) {
-  		$(".swipebox").swipebox();
-		$(".videocontainer").fitVids();
-		
-		$("#ContactForm").validate({
-		submitHandler: function(form) {
-		ajaxContact(form);
-		return false;
-		}
-		});
-		
 
-		$(".posts li").hide();	
-		size_li = $(".posts li").size();
-		x=3;
-		$('.posts li:lt('+x+')').show();
-		$('#loadMore').click(function () {
-			x= (x+1 <= size_li) ? x+1 : size_li;
-			$('.posts li:lt('+x+')').show();
-			if(x == size_li){
-				$('#loadMore').hide();
-				$('#showLess').show();
-			}
-		});
+// Callbacks to run specific code for specific pages, for example for About page:
+myApp.onPageInit('about', function (page) {
+    // run createContentPage func after link was clicked
+    $$('.create-page').on('click', function () {
+        createContentPage();
+    });
+});
 
-
-		
-	$("a.switcher").bind("click", function(e){
-		e.preventDefault();
-		
-		var theid = $(this).attr("id");
-		var theproducts = $("ul#photoslist");
-		var classNames = $(this).attr('class').split(' ');
-		
-		
-		if($(this).hasClass("active")) {
-			// if currently clicked button has the active class
-			// then we do nothing!
-			return false;
-		} else {
-			// otherwise we are clicking on the inactive button
-			// and in the process of switching views!
-
-  			if(theid == "view13") {
-				$(this).addClass("active");
-				$("#view11").removeClass("active");
-				$("#view11").children("img").attr("src","images/switch_11.png");
-				
-				$("#view12").removeClass("active");
-				$("#view12").children("img").attr("src","images/switch_12.png");
-			
-				var theimg = $(this).children("img");
-				theimg.attr("src","images/switch_13_active.png");
-			
-				// remove the list class and change to grid
-				theproducts.removeClass("photo_gallery_11");
-				theproducts.removeClass("photo_gallery_12");
-				theproducts.addClass("photo_gallery_13");
-
-			}
-			
-			else if(theid == "view12") {
-				$(this).addClass("active");
-				$("#view11").removeClass("active");
-				$("#view11").children("img").attr("src","images/switch_11.png");
-				
-				$("#view13").removeClass("active");
-				$("#view13").children("img").attr("src","images/switch_13.png");
-			
-				var theimg = $(this).children("img");
-				theimg.attr("src","images/switch_12_active.png");
-			
-				// remove the list class and change to grid
-				theproducts.removeClass("photo_gallery_11");
-				theproducts.removeClass("photo_gallery_13");
-				theproducts.addClass("photo_gallery_12");
-
-			} 
-			else if(theid == "view11") {
-				$("#view12").removeClass("active");
-				$("#view12").children("img").attr("src","images/switch_12.png");
-				
-				$("#view13").removeClass("active");
-				$("#view13").children("img").attr("src","images/switch_13.png");
-			
-				var theimg = $(this).children("img");
-				theimg.attr("src","images/switch_11_active.png");
-			
-				// remove the list class and change to grid
-				theproducts.removeClass("photo_gallery_12");
-				theproducts.removeClass("photo_gallery_13");
-				theproducts.addClass("photo_gallery_11");
-
-			} 
-			
-		}
-
-	});	
-	
-	document.addEventListener('touchmove', function(event) {
-	   if(event.target.parentNode.className.indexOf('navbarpages') != -1 || event.target.className.indexOf('navbarpages') != -1 ) {
-		event.preventDefault(); }
-	}, false);
-	
-	// Add ScrollFix
-	var scrollingContent = document.getElementById("pages_maincontent");
-	new ScrollFix(scrollingContent);
-	
-	
-	var ScrollFix = function(elem) {
-		// Variables to track inputs
-		var startY = startTopScroll = deltaY = undefined,
-	
-		elem = elem || elem.querySelector(elem);
-	
-		// If there is no element, then do nothing	
-		if(!elem)
-			return;
-	
-		// Handle the start of interactions
-		elem.addEventListener('touchstart', function(event){
-			startY = event.touches[0].pageY;
-			startTopScroll = elem.scrollTop;
-	
-			if(startTopScroll <= 0)
-				elem.scrollTop = 1;
-	
-			if(startTopScroll + elem.offsetHeight >= elem.scrollHeight)
-				elem.scrollTop = elem.scrollHeight - elem.offsetHeight - 1;
-		}, false);
-	};
-	
-		
-		
-})
+// Generate dynamic page
+var dynamicPageIndex = 0;
+function createContentPage() {
+	mainView.router.loadContent(
+        '<!-- Top Navbar-->' +
+        '<div class="navbar">' +
+        '  <div class="navbar-inner">' +
+        '    <div class="left"><a href="#" class="back link"><i class="icon icon-back"></i><span>Back</span></a></div>' +
+        '    <div class="center sliding">Dynamic Page ' + (++dynamicPageIndex) + '</div>' +
+        '  </div>' +
+        '</div>' +
+        '<div class="pages">' +
+        '  <!-- Page, data-page contains page name-->' +
+        '  <div data-page="dynamic-pages" class="page">' +
+        '    <!-- Scrollable page content-->' +
+        '    <div class="page-content">' +
+        '      <div class="content-block">' +
+        '        <div class="content-block-inner">' +
+        '          <p>Here is a dynamic page created on ' + new Date() + ' !</p>' +
+        '          <p>Go <a href="#" class="back">back</a> or go to <a href="services.html">Services</a>.</p>' +
+        '        </div>' +
+        '      </div>' +
+        '    </div>' +
+        '  </div>' +
+        '</div>'
+    );
+	return;
+}
